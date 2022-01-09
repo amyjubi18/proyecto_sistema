2e17bbb0ed3623352a4c60bba7c75f21878bf2a2
amyjubi18 <59567217+amyjubi18@users.noreply.github.com>
2022-01-09 18:19:35 -0400

    Add files via upload

--- controldeacceso.sql
+++ controldeacceso.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-01-2022 a las 06:47:50
+-- Tiempo de generación: 09-01-2022 a las 23:09:27
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.3.0
 
@@ -62,13 +62,53 @@ INSERT INTO `carrera` (`cod_carrera`, `carreras`) VALUES
 
 CREATE TABLE `estudiantes` (
   `id_estudiantes` int(10) NOT NULL,
-  `matricula` int(10) NOT NULL,
+  `periodo_id` int(10) NOT NULL,
   `nombre_est` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `apellido_est` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `cedula_est` int(10) NOT NULL,
-  `cod_carrera` int(10) NOT NULL,
-  `turno` varchar(15) COLLATE utf8_spanish_ci NOT NULL
+  `cod_carrera` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `estudiantes`
+--
+
+INSERT INTO `estudiantes` (`id_estudiantes`, `periodo_id`, `nombre_est`, `apellido_est`, `cedula_est`, `cod_carrera`) VALUES
+(3, 20141, 'gugu', 'fxkgjhsdh', 4545, 1022),
+(4, 20142, 'amanda', 'garcia', 25565678, 1026);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `periodo`
+--
+
+CREATE TABLE `periodo` (
+  `periodo_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `periodo`
+--
+
+INSERT INTO `periodo` (`periodo_id`) VALUES
+(20141),
+(20142),
+(20151),
+(20152),
+(20161),
+(20162),
+(20171),
+(20172),
+(20181),
+(20182),
+(20191),
+(20192),
+(20201),
+(20202),
+(20211),
+(20212),
+(20221);
 
 --
 -- Índices para tablas volcadas
@@ -85,7 +125,15 @@ ALTER TABLE `carrera`
 --
 ALTER TABLE `estudiantes`
   ADD PRIMARY KEY (`id_estudiantes`),
-  ADD KEY `cod_carrera` (`cod_carrera`);
+  ADD KEY `cod_carrera` (`cod_carrera`),
+  ADD KEY `periodo` (`periodo_id`),
+  ADD KEY `periodo_id` (`periodo_id`);
+
+--
+-- Indices de la tabla `periodo`
+--
+ALTER TABLE `periodo`
+  ADD PRIMARY KEY (`periodo_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -95,7 +143,7 @@ ALTER TABLE `estudiantes`
 -- AUTO_INCREMENT de la tabla `estudiantes`
 --
 ALTER TABLE `estudiantes`
-  MODIFY `id_estudiantes` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_estudiantes` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
@@ -105,7 +153,8 @@ ALTER TABLE `estudiantes`
 -- Filtros para la tabla `estudiantes`
 --
 ALTER TABLE `estudiantes`
-  ADD CONSTRAINT `estudiantes_ibfk_1` FOREIGN KEY (`cod_carrera`) REFERENCES `carrera` (`cod_carrera`);
+  ADD CONSTRAINT `estudiantes_ibfk_1` FOREIGN KEY (`cod_carrera`) REFERENCES `carrera` (`cod_carrera`),
+  ADD CONSTRAINT `estudiantes_ibfk_2` FOREIGN KEY (`periodo_id`) REFERENCES `periodo` (`periodo_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
